fe914947e757b4b85b88f52d2611513ffc76b380
Cameron Pettit <71421099+cameronpettit@users.noreply.github.com>
2021-06-08 16:37:56 -0700

    add alert component (#13)

---
+++ src/app/shared/components/alert/alert.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { AlertObject } from './alert-object';
+
+@Component({
+  selector: 'app-alert',
+  templateUrl: './alert.component.html',
+  styleUrls: ['./alert.component.scss']
+})
+export class AlertComponent implements OnInit {
+  @Input() data: AlertObject;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    console.log(this.data);
+  }
+
+  getAlertClass(type): string {
+    switch (type) {
+      case 'error':
+        return 'error-container';
+      case 'warning':
+        return 'warning-container';
+      case 'success':
+        return 'success-container';
+      case 'info':
+      default:
+        return 'info-container';
+    }
+  }
+
+}
