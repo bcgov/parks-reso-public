6e77d74d74216c4c0b91137747fcff33eb896e7e
Cameron <cameron@digitalspace.ca>
2021-06-14 12:47:22 -0700

    pass reservation submission
    removing console logs
    
    fixing issue with pass submission
    
    Various clean up items (#23)
    
    Various clean up items (#23)

--- src/app/registration/success/success.component.ts
+++ src/app/registration/success/success.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { PassService } from 'src/app/services/pass.service';
 
 @Component({
   selector: 'app-success',
@@ -9,20 +10,21 @@ import { Router } from '@angular/router';
 export class SuccessComponent implements OnInit {
   @Input() regData;
   constructor(
-    private router: Router
+    private router: Router,
+    private passService: PassService
   ) { }
 
   ngOnInit(): void {
   }
 
   print(): void {
-    const printContent = document.getElementById('success');
+    const printContent = document.getElementById('registration');
     const WindowPrt = window.open('', '', 'left=0,top=0,width=900,height=900,toolbar=0,scrollbars=0,status=0');
+    WindowPrt.document.write('<h4> BC Parks Day Pass Reservation System </h4>');
     WindowPrt.document.write(printContent.innerHTML);
     WindowPrt.document.close();
     WindowPrt.focus();
     WindowPrt.print();
-    WindowPrt.close();
   }
 
   navigate(): void {
