ad6d4901722c8951eff1ea05795c5e16bd01096e
Cameron <cameron@digitalspace.ca>
2021-06-25 14:27:49 -0700

    phone field restrictions

--- src/app/registration/contact-form/contact-form.component.ts
+++ src/app/registration/contact-form/contact-form.component.ts
@@ -51,6 +51,17 @@ export class ContactFormComponent implements OnInit {
     }
   }
 
+  keyPressNumbers(event) {
+    var charCode = (event.which) ? event.which : event.keyCode;
+    // Only Numbers 0-9
+    if ((charCode < 48 || charCode > 57)) {
+      event.preventDefault();
+      return false;
+    } else {
+      return true;
+    }
+  }
+
   getMonthString(monthNo): string {
     return this.months[monthNo - 1];
   }
