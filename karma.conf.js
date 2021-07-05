ee50339e80026dea6f47b09b526d512cc9e560c6
Daniel Truong <daniel@dtsoftware.ca>
2021-07-05 16:41:47 -0700

    Gracefully exit when test is complete

--- karma.conf.js
+++ karma.conf.js
@@ -37,6 +37,6 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadless'],
+    browsers: ['ChromeHeadless']
   });
 };
