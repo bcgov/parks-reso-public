500b3b83f6d59078e9202b711fd01db40606e7bf
BcGovNeal <47604552+BcGovNeal@users.noreply.github.com>
2021-11-25 09:48:17 -0800

    BRS-94 Update contact info page (#69)

--- src/app/registration/contact-form/contact-form.component.spec.ts
+++ src/app/registration/contact-form/contact-form.component.spec.ts
@@ -1,4 +1,6 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ConfigService } from 'src/app/services/config.service';
 import { SharedModule } from 'src/app/shared/shared.module';
 
 import { ContactFormComponent } from './contact-form.component';
@@ -10,7 +12,8 @@ describe('ContactFormComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [],
-      imports: [SharedModule]
+      imports: [SharedModule, HttpClientTestingModule],
+      providers: [ConfigService]
     })
     .compileComponents();
   });
