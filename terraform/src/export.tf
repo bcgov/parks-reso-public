ac99e68993c8233b1865c17b57b7b5baf0bf48a4
Daniel Truong <daniel@digitalspace.ca>
2022-05-26 16:21:46 -0700

    BRS-501: fixing CORS issues (#41)

--- terraform/src/export.tf
+++ terraform/src/export.tf
@@ -44,6 +44,56 @@ resource "aws_api_gateway_method" "exportGet" {
   authorization = "NONE"
 }
 
+//CORS
+resource "aws_api_gateway_method" "export_options_method" {
+  rest_api_id   = aws_api_gateway_rest_api.apiLambda.id
+  resource_id   = aws_api_gateway_resource.exportResource.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method_response" "export_options_200" {
+  rest_api_id = aws_api_gateway_rest_api.apiLambda.id
+  resource_id = aws_api_gateway_resource.exportResource.id
+  http_method = aws_api_gateway_method.export_options_method.http_method
+  status_code = "200"
+  response_models = {
+    "application/json" = "Empty"
+  }
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+  depends_on = [aws_api_gateway_method.export_options_method]
+}
+
+resource "aws_api_gateway_integration" "export_options_integration" {
+  rest_api_id = aws_api_gateway_rest_api.apiLambda.id
+  resource_id = aws_api_gateway_resource.exportResource.id
+  http_method = aws_api_gateway_method.export_options_method.http_method
+  type        = "MOCK"
+  request_templates = {
+    "application/json" : "{\"statusCode\": 200}"
+  }
+  depends_on = [aws_api_gateway_method.export_options_method]
+}
+
+resource "aws_api_gateway_integration_response" "export_options_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.apiLambda.id
+  resource_id = aws_api_gateway_resource.exportResource.id
+  http_method = aws_api_gateway_method.export_options_method.http_method
+
+  status_code = aws_api_gateway_method_response.export_options_200.status_code
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,POST,OPTIONS'",
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+  depends_on = [aws_api_gateway_method_response.export_options_200]
+}
+
+
 resource "aws_api_gateway_integration" "exportGetIntegration" {
   rest_api_id = aws_api_gateway_rest_api.apiLambda.id
   resource_id = aws_api_gateway_resource.exportResource.id
