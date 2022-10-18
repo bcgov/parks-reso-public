d3fea7d36981a8f243e4eadd93f3c798d1c67ee4
Mark Lisé <marklise@users.noreply.github.com>
2022-10-18 09:40:37 -0700

    BRS-810: Service workers. (#182)
    * BRS-810: Service workers.
    
    * Fix UT's

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,8 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, throwError } from 'rxjs';
 import { ConfigService } from '../shared/services/config.service';
-import { LoggerService } from './logger.service';
+import { SwUpdate } from '@angular/service-worker';
+import { ToastService } from './toast.service';
+import { Constants } from '../shared/utils/constants';
 
 @Injectable({
   providedIn: 'root'
@@ -15,13 +16,38 @@ export class ApiService {
   env: 'local' | 'dev' | 'test' | 'prod';
 
   constructor(
+    readonly updates: SwUpdate,
+    private toastService: ToastService,
     private http: HttpClient,
-    private configService: ConfigService,
-    private logger: LoggerService
+    private configService: ConfigService
   ) {
-    this.apiPath = this.configService.config['API_LOCATION']
-      + this.configService.config['API_PUBLIC_PATH'];
+    this.apiPath = this.configService.config['API_LOCATION'] + this.configService.config['API_PUBLIC_PATH'];
     this.env = this.configService.config['ENVIRONMENT'];
+
+    console.log(`Update checking enabled? (${updates.isEnabled})`);
+
+    updates.versionUpdates.subscribe(evt => {
+      switch (evt.type) {
+        case 'VERSION_DETECTED':
+          console.log(`Downloading new app version: ${evt.version.hash}`);
+          break;
+        case 'VERSION_READY':
+          console.log(`Current app version: ${evt.currentVersion.hash}`);
+          console.log(`New app version ready for use: ${evt.latestVersion.hash}`);
+          this.toastService.addMessage(
+            `Please reload your browser, there is an update available.`,
+            `Update detected`,
+            Constants.ToastTypes.INFO
+          );
+
+          break;
+        case 'VERSION_INSTALLATION_FAILED':
+          console.log(`Failed to install app version '${evt.version.hash}': ${evt.error}`);
+          break;
+        case 'NO_NEW_VERSION_DETECTED':
+          console.log('No new version detected.')
+      }
+    });
   }
 
   post(endpoint, obj, queryParamsObject = null): Promise<any> {
