040aad6272e3d6d44169d497ac996f5a5bb5b975
Mark Lisé <mark@digitalspace.ca>
2022-12-09 16:05:02 -0800

    NOBUG: UT coverage for card component.

--- src/app/card/card.component.spec.ts
+++ src/app/card/card.component.spec.ts
@@ -1,6 +1,8 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Location } from "@angular/common";
+import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { RegistrationComponent } from '../registration/registration.component';
 import { ConfigService } from '../shared/services/config.service';
 
 import { CardComponent } from './card.component';
@@ -8,23 +10,54 @@ import { CardComponent } from './card.component';
 describe('CardComponent', () => {
   let component: CardComponent;
   let fixture: ComponentFixture<CardComponent>;
+  let imgtag: HTMLElement;
+  let mockConfigService;
+  let location: Location;
 
   beforeEach(async () => {
+
+    mockConfigService = {
+      config: {
+        "ASSETS_S3_URL": "http://local-env/foo"
+      }
+    }
+
     await TestBed.configureTestingModule({
       declarations: [CardComponent],
-      imports: [RouterTestingModule, HttpClientTestingModule],
-      providers: [ConfigService]
-    })
-      .compileComponents();
-  });
-
-  beforeEach(() => {
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule,
+        RouterTestingModule.withRoutes(
+          [{ path: 'registration', component: RegistrationComponent }]
+        )
+      ],
+      providers: [ {
+        provide: ConfigService, useValue: mockConfigService
+      }]
+    }).compileComponents();
     fixture = TestBed.createComponent(CardComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    component.data = { "name": "Some Name", "sk": "SomeSK"};
+    location = TestBed.inject(Location);
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should create and set the alt tag', () => {
+    fixture.detectChanges();
+    imgtag = fixture.debugElement.nativeElement.querySelector('img');
+    expect(component.altText).toBe(imgtag.getAttribute('alt'))
   });
+
+  it('should show name on the H2 and show the URL properly', async () => {
+    expect(component).toBeTruthy();
+    component.ngOnInit();
+    await fixture.whenStable();
+    fixture.detectChanges();
+    expect(fixture.debugElement.nativeElement.querySelector('h2').innerHTML).toEqual('Some Name');
+  });
+
+  it('should navigate to registration page', fakeAsync(() => {
+    component.navigate('park');
+    tick(50)
+    expect(location.path()).toBe("/registration");
+  }));
 });
