c03cd9209c28418b5a96ea08bca11fe0fb371db7
Mark Lisé <marklise@users.noreply.github.com>
2022-12-13 16:05:13 -0800

    NOBUG: Adding cc to ut's. (#200)

--- src/app/breadcrumb/breadcrumb.component.spec.ts
+++ src/app/breadcrumb/breadcrumb.component.spec.ts
@@ -1,22 +1,63 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
+import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
+import { of } from 'rxjs/internal/observable/of';
 
 import { BreadcrumbComponent } from './breadcrumb.component';
 
 describe('BreadcrumbComponent', () => {
   let component: BreadcrumbComponent;
+  let activatedRoute: ActivatedRoute;
+  let router: Router;
   let fixture: ComponentFixture<BreadcrumbComponent>;
-
+  let eventsSub = new BehaviorSubject<any>(null);
+  let routerStub = {
+    events: eventsSub,
+  };
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [BreadcrumbComponent],
-      imports: [RouterTestingModule]
-    })
-      .compileComponents();
-  });
-
-  beforeEach(() => {
+      imports: [
+        RouterTestingModule,
+        RouterTestingModule.withRoutes(
+          [
+            {
+              path: 'home',
+              component: BreadcrumbComponent,
+              children: [
+                {
+                  path: 'something/else',
+                  component: BreadcrumbComponent
+                }
+              ]
+            }
+          ]
+        )
+      ],
+      providers: [
+        {
+          provide: Router, useValue: routerStub
+        },
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              params: {
+                    property: 'yourProperty',
+                    someId: 3
+                  },
+             queryParams: of({})
+            },
+            queryParams: of({}),
+            queryParamMap: of({}),
+            params: of({id: 'something'})
+          }
+        }
+      ]
+    }).compileComponents();
+    activatedRoute = TestBed.inject(ActivatedRoute);
+    router = TestBed.inject(Router);
     fixture = TestBed.createComponent(BreadcrumbComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -24,5 +65,25 @@ describe('BreadcrumbComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit breadcrumbs', async () => {
+    const url = 'http://localhost/foo';
+    const params =  { someParam: '123'};
+    expect(component).toBeTruthy();
+    spyOn(component.navigateBreadcrumb, 'emit');
+    component.emitBreadcrumb(url, params);
+    fixture.detectChanges();
+    expect(component.navigateBreadcrumb.emit).toHaveBeenCalledWith({url, params});
+  });
+
+  it('Init, subscribe and get breadcrumbs', async () => {
+    expect(component).toBeTruthy();
+    component.ngOnInit();
+    await fixture.whenStable();
+    fixture.detectChanges();
+    let mockNav = new NavigationEnd(1, 'home', 'home');
+    eventsSub.next(mockNav);
+    fixture.detectChanges();
   });
 });
