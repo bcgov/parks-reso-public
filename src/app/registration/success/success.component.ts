25ce4be17e42eb62faa39f69dd279fa6ffb46fb8
Daniel Truong <daniel@digitalspace.ca>
2023-01-14 00:17:17 +0000

    BRS-1000: fixing print button for QR

--- src/app/registration/success/success.component.ts
+++ src/app/registration/success/success.component.ts
@@ -21,9 +21,13 @@ export class SuccessComponent implements OnInit {
   }
 
   print(): void {
+    const qrContent = document.getElementById('qr-code');
     const printContent = document.getElementById('registration');
     const WindowPrt = window.open('', '', 'left=0,top=0,width=900,height=900,toolbar=0,scrollbars=0,status=0');
     WindowPrt.document.write('<h4> BC Parks Day Pass Reservation System </h4>');
+    if (qrContent) {
+      WindowPrt.document.write(qrContent.innerHTML);
+    }
     WindowPrt.document.write(printContent.innerHTML);
     WindowPrt.document.close();
     WindowPrt.focus();
