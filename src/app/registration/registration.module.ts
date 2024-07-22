1e2c4802f8ea978e09500a76c9e189e52dc1ca9f
cameron <cameron@digitalspace.ca>
2024-07-22 14:39:48 -0700

    BRS-345 Joffre specific messaging on DUP confirmation page

--- src/app/registration/registration.module.ts
+++ src/app/registration/registration.module.ts
@@ -17,6 +17,7 @@ import { NgxIntlTelInputModule } from '@moddi3/ngx-intl-tel-input';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgxTurnstileModule } from "ngx-turnstile";
 import { TimerComponent } from './timer/timer.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 @NgModule({
   declarations: [
     RegistrationComponent,
@@ -39,7 +40,8 @@ import { TimerComponent } from './timer/timer.component';
     QRCodeModule,
     NgxIntlTelInputModule,
     BrowserAnimationsModule,
-    NgxTurnstileModule
+    NgxTurnstileModule,
+    FontAwesomeModule
   ],
   providers: []
 })
