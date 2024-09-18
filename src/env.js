daaee19f099f2ccc72db825db7d061004c4c9e54
Christopher-walsh22 <106549296+Christopher-walsh22@users.noreply.github.com>
2024-09-18 13:30:56 -0700

    BRS:438 Convert Day Use front ends to use SAM (#452)
    * Sam Changes
    
    * Wrong path in workflow

--- src/env.js
+++ src/env.js
@@ -4,17 +4,19 @@
   window.__env.logLevel = 0; // All
 
   // Get config from remote host?
-  window.__env.configEndpoint = false;
+  window.__env.configEndpoint = true;
   window.__env.hashVersion = 'local';
 
   // Environment name
   window.__env.ENVIRONMENT = 'local';
 
   window.__env.API_LOCATION = 'http://localhost:3000';
-  window.__env.API_PATH = '/api';
-  window.__env.API_PUBLIC_PATH = '/api';
+  window.__env.API_PATH = '/';
+  window.__env.API_PUBLIC_PATH = '/';
   window.__env.ASSETS_S3_URL = 'https://d3ptawfth8sh71.cloudfront.net';
 
+
+  //Always reject requests, update with required key as needed for Local.
   window.__env.TURNSTILE_SITE_KEY = '2x00000000000000000000AB';
 
   // Add any feature-toggles
