63d0c452ea189b06141a74315edea51b5d3c9b49
Christopher-walsh22 <106549296+Christopher-walsh22@users.noreply.github.com>
2026-05-05 22:38:55 +0000

    Fix test

--- src/app/pass-lookup/pass-lookup-form/pass-lookup-form.component.spec.ts
+++ src/app/pass-lookup/pass-lookup-form/pass-lookup-form.component.spec.ts
@@ -1,16 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
+import { of } from 'rxjs';
 
 import { PassLookupFormComponent } from './pass-lookup-form.component';
+import { ParkService } from '../../services/park.service';
 
 describe('PassLookupFormComponent', () => {
   let component: PassLookupFormComponent;
   let fixture: ComponentFixture<PassLookupFormComponent>;
+  let mockParkService: jasmine.SpyObj<ParkService>;
 
   beforeEach(async () => {
+    mockParkService = jasmine.createSpyObj('ParkService', ['fetchData', 'getItemValue']);
+    mockParkService.getItemValue.and.returnValue(of(null));
+
     await TestBed.configureTestingModule({
       imports: [ ReactiveFormsModule ],
-      declarations: [ PassLookupFormComponent ]
+      declarations: [ PassLookupFormComponent ],
+      providers: [
+        { provide: ParkService, useValue: mockParkService }
+      ]
     })
     .compileComponents();
   });
